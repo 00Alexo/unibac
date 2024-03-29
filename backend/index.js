8b39effe1425b9e2c271a73a32665a2e739f2718
Alex <alexsuciultc200@gmail.com>
2024-03-29 13:26:04 +0100

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const requestIp = require('request-ip');
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://unibac.vercel.app']
+  }))
+app.use(requestIp.mw());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    if (req.url === '/' && req.method === 'GET') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Wassup amigo');
+    }
+    next();
+ })
+
+mongoose.connect(process.env.mongoDB)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection failed:", error);
+}); 
+
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT);
+  });
