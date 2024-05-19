ce4556c3172fb67a28b5a4d6ba8a31ca880fcbf0
Alexo <alexsuciultc200@gmail.com>
2024-05-19 17:59:47 +0300

    dasdagdfahgafh

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const userRoutes = require('./routes/userRoutes')
 const app = express();
 
 app.use((req, res, next) => {
-  const allowedOrigin = 'https://unibac.vercel.app';
-  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+  res.setHeader("Access-Control-Allow-Origin", 'https://unibac.vercel.app');
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, username");
   res.setHeader("Access-Control-Allow-Credentials", "true");
