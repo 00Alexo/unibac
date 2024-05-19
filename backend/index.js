d40f149a4f57c922977b1615cf63e75152ccbfed
Alexo <alexsuciultc200@gmail.com>
2024-05-19 16:09:53 +0300

    BLOODEH CORS

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,11 @@ const userRoutes = require('./routes/userRoutes')
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://unibac.vercel.app', 'https://infocitadeltest.onrender.com']
-  }))
+  origin: ['http://localhost:3000', 'https://unibac.vercel.app', 'https://infocitadeltest.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // if you need to include cookies in the requests
+}));
 app.use(requestIp.mw());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
