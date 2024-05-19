76a0bcbcb13f98cdf8c1c19b40f1bd6d8ac4535f
Alexo <alexsuciultc200@gmail.com>
2024-05-19 17:36:54 +0300

    FIUAJFADJGFAIDGJAIO CORS

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,14 @@ const requestIp = require('request-ip');
 const userRoutes = require('./routes/userRoutes')
 const app = express();
 
-app.use(cors({
-  origin: ['https://unibac.vercel.app']
-}))
+app.use((req, res, next) => {
+  const allowedOrigin = 'http://localhost:3000';
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(requestIp.mw());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
